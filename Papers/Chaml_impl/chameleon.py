4f1e37839a052f744a313a90d7e82404b7d06127
kshitij <kshitijagrawal2607@gmail.com>
2020-03-23 13:22:59 +0530

    Implemented Accuracy metric

--- Papers/Chaml_impl/chameleon.py
+++ Papers/Chaml_impl/chameleon.py
@@ -1,5 +1,6 @@
 from getdata import data
 import os
+import pprint
 
 class chameleon:
     def __init__(self):
@@ -24,9 +25,10 @@ class chameleon:
             if file.endswith('.mkv'):
                 files.append(file)
         itr = 0
-        for file in files:
-            self.getFrames(os.path.join(path, file), 10, 'res0_' + str(itr))
-            itr += 1
+        # for file in files:
+        #     self.getFrames(os.path.join(path, file), 10, 'res0_' + str(itr))
+        #     itr += 1
+        self.getF1score(1, 1)
     
     def getFrames(self, file, frame = 0, outfile = 'res0'):
         os.system('ffmpeg -i {} -vf "select=eq(n\,{})" -vframes 1 {}.jpg'.format(file, frame, outfile))
@@ -65,40 +67,74 @@ class chameleon:
         # Todo : See how to set params for this 
         return        
 
-    def getbbox(config):
+    def getbbox(self, file):
         # Redirect YOLO's output to a file having a list of dicts
-        # returns : dict - [{keys : x1, x2, y1, y2}]
-        return
+        # returns : list of tuples (Class, {Bounding Box : x1, x2, y1, y2})
+        bounding_boxes = list()
+        prev = ''
+        curr = ''
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            prev = ''
+            curr = ''
+            lines = lines[1:]
+            for n in range(0, len(lines)):
+                if n % 2 == 1:
+                    box = [(int)(x) for x in lines[n].split()]
+                    obj = lines[n - 1].split(':')[0]
+                    grp = (obj, box)
+                    bounding_boxes.append(grp)
+
+        return bounding_boxes
 
     def getF1score(self, curr_config, golden_config):
         # Run for all bounding boxes and calculate accuracy
+        path = './darknet/'
+        file_golden = ''
+        file_current = ''
+        for file in os.listdir(path):
+            if file.endswith('.txt'):
+                if file.endswith('base.txt'):
+                    file_golden = file
+                else:
+                    file_current = file
         precision = 0.0
         recall = 0.0
+        curr_bboxes = self.getbbox(os.path.join(path, file_current))
+        gold_bboxes = self.getbbox(os.path.join(path, file_golden))
+        total_detect = len(gold_bboxes)
+        total_object = len(curr_bboxes)
+        true_positive = 0
+        # pprint.pprint(gold_bboxes)
+        # pprint.pprint(curr_bboxes)
+        for box_1 in curr_bboxes:
+            for box_2 in gold_bboxes:
+                if box_1[0] == box_2[0]:
+                    x_left = max(box_1[1][0], box_2[1][0])
+                    y_top = max(box_1[1][1], box_2[1][1])
+                    x_right = min(box_1[1][2], box_2[1][2])
+                    y_bottom = min(box_1[1][3], box_2[1][3])
 
-        curr_bboxes = self.getbbox(curr_config)
-        gold_bboxes = self.getbbox(golden_config)
+                    if(x_left > x_right or y_top > y_bottom):
+                        continue
 
-        bb1 = self.getbbox(curr_config)
-        bb2 = self.getbbox(golden_config)
+                    intersection = (x_right - x_left) * (y_bottom - y_top)
 
-        x_left = max(bb1['x1'], bb2['x1'])
-        y_top = max(bb1['y1'], bb2['y1'])
-        x_right = min(bb1['x2'], bb2['x2'])
-        y_bottom = min(bb1['y2'], bb2['y2'])
+                    A1 = (box_1[1][2] - box_1[1][0]) * (box_1[1][3] - box_1[1][1])
+                    A2 = (box_2[1][2] - box_2[1][0]) * (box_2[1][3] - box_2[1][1])
 
-        if(x_left > x_right or y_top > y_bottom):
-            return 0.0
+                    IoU = (intersection) / (A1 + A2 - intersection)
+                    assert IoU >= 0.0
+                    assert IoU <= 1.0
+                    if IoU >= self.threshold:
+                        true_positive += 1
+                        gold_bboxes.remove(box_2)
+                        break
 
-        intersection = (x_right - x_left) * (y_bottom - y_top)
-
-        A1 = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
-        A2 = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
-
-        IoU = (intersection) / (A1 + A2 - intersection)
-        assert IoU >= 0.0
-        assert IoU <= 1.0
-
-        return
+        precision = true_positive / total_detect
+        recall = true_positive / total_object
+        F1 = 2 / ((1 / precision) + (1 / recall))
+        return F1
 
 if __name__ == "__main__":
     chameleon()
